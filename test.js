bfef50f83f49e672782b4c06c1ab3ab86fac56fe
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-05-13 10:00:20 -0400

    fixed tape name

--- test.js
+++ test.js
@@ -2,12 +2,14 @@ var test = require('tape')
 var fs = require('fs')
 var tin = require('./')
 
-test('tag', function(t){
+test('tin', function(t){
   var points = JSON.parse(fs.readFileSync(__dirname+'/geojson/Points.geojson'))
   
   var tinned = tin(points, 'elevation')
 
   t.equal(tinned.features[0].geometry.type, 'Polygon')
-  console.log(tinned.features.length)
+  t.equal(tinned.features.length, 24)
+
+  fs.writeFileSync(__dirname+'/geojson/Tin.geojson', JSON.stringify(tinned))
   t.end()
 });
